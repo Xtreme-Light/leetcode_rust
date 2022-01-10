a6f86cd9fd2317742cc9327b77c9dbcaf595ba71
Xtreme-Light <whateverxl@outlook.com>
2022-01-10 23:37:05 +0800

    feature: 提交小偷问题全家桶

---
+++ src/dp/delete_and_earn.rs
@@ -0,0 +1,40 @@
+use crate::Solution;
+use std::collections::HashMap;
+
+impl Solution {
+    /// day3
+    /// https://leetcode-cn.com/problems/delete-and-earn/
+    ///
+    /// 给你一个整数数组 nums ，你可以对它进行一些操作。
+    ///
+    /// 每次操作中，选择任意一个 nums[i] ，删除它并获得 nums[i] 的点数。之后，你必须删除 所有 等于 nums[i] - 1 和 nums[i] + 1 的元素。
+    ///
+    /// 开始你拥有 0 个点数。返回你能通过这些操作获得的最大点数。
+    ///
+    pub fn delete_and_earn(nums: Vec<i32>) -> i32 {
+        let mut n = *nums.iter().max().unwrap() as usize;
+        // 记录各个数字出现的次数
+        let mut arr = vec![0; n + 1];
+        for i in 0..nums.len() {
+            arr[nums[i] as usize] += 1;
+        }
+        let mut pick = 0;
+        let mut ban = 0;
+        for i in 0..arr.len() {
+            let pre_pick = pick;
+            pick = pick.max(ban + arr[i] * i);
+            ban = pre_pick;
+        }
+        pick as i32
+    }
+}
+#[cfg(test)]
+mod test {
+    use crate::Solution;
+
+    #[test]
+    pub fn test_delete_and_earn() {
+        assert_eq!(Solution::delete_and_earn(vec![3, 4, 2]), 6);
+        assert_eq!(Solution::delete_and_earn(vec![2, 2, 3, 3, 3, 4]), 9);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,5 @@ mod majority_element;
 mod search_matrix;
 mod single_number;
 
-fn main() {
-    println!("Hello, world!");
-    for i in 0..5 {
-        println!("this is {}", i);
-    }
-    let n = 2;
-    println!("xxx {}", n >> 1);
-}
+fn main() {}
 struct Solution;
